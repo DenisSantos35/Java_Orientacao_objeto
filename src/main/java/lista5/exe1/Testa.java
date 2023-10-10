58da89b9d36091a6d537456d18aa038f3bd0c508
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2023-10-10 08:11:39 -0300

    interface em java

---
+++ src/main/java/lista5/exe1/Testa.java
@@ -0,0 +1,12 @@
+
+package lista5.exe1;
+
+public class Testa {
+
+    public static void main(String[] args) {
+        JogadorBasquete objJb = new AtletaBasqueteFutebol();
+        
+        AtletaBasqueteFutebol objBF = new AtletaBasqueteFutebol();
+    }
+    
+}
